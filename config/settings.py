861959fa822188059b03a494463426c30c5041bc
Shani Gupta <shanigupta90058@gmail.com>
2026-08-28 00:42:55 +0530

    Fix Railway allowed host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = 'django-insecure-o@nqb6dx#o2hcd%49ax8wfe%@$=)i1*+=w9(d3#4x#8)m4ycsl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "buildconnect-production.up.railway.app",
+]
 
 
 # Application definition
